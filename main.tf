90b3c7f86dfff020c7403d94caeccf99579b4579
Joris <contact@hydeos.fr>
2024-07-11 17:43:31 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "azurerm_service_plan" "TP_Azure" {
   resource_group_name = azurerm_resource_group.Azure.name
   location            = "westeurope"
   os_type             = "Linux"
-  sku_name            = "F1"
+  sku_name            = "S3"
 }
 
 resource "azurerm_linux_web_app" "linux-azure" {
