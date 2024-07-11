65658f8156e7b574d3b6364788d91699ca7c2030
Joris <contact@hydeos.fr>
2024-07-11 21:26:12 +0200

    update

--- index.php
+++ index.php
@@ -5,71 +5,83 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-     
+    
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker.min.css" rel="stylesheet">
     
     <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
     
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: white;
+            font-family: Arial, sans-serif;
+        }
+        #countdown {
+            font-size: 3em;
+        }
+    </style>
+
     <script type="text/javascript">
-        
-        $(document).ready(function(e)
-        {
-            // Initialiser le datepicker (https://github.com/eternicode/bootstrap-datepicker)
-            $('#datepicker').datepicker(
-            {
+        $(document).ready(function() {
+            $('#datepicker').datepicker({
                 orientation: "top auto",
                 autoclose: true,
                 format: "dd/mm/yyyy",
                 language: "fr"
             });
+
+            $('form').on('submit', function(e) {
+                e.preventDefault();
+                var dateStr = $('#datepicker').val();
+                var eventName = $('input[name="event"]').val();
+                if (dateStr && eventName) {
+                    var dateParts = dateStr.split('/');
+                    var targetDate = new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
+                    $('#form-container').hide();
+                    $('#countdown').show();
+                    startCountdown(targetDate, eventName);
+                }
+            });
+
+            function startCountdown(targetDate, eventName) {
+                function updateCountdown() {
+                    var now = new Date().getTime();
+                    var distance = targetDate - now;
+
+                    if (distance < 0) {
+                        clearInterval(countdownInterval);
+                        $('#countdown').text(`${eventName} a déjà eu lieu !`);
+                        return;
+                    }
+
+                    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+                    $('#countdown').text(`Il reste ${days}j ${hours}h ${minutes}m ${seconds}s avant ${eventName}`);
+                }
+
+                updateCountdown();
+                var countdownInterval = setInterval(updateCountdown, 1000);
+            }
         });
-        
     </script>
 </head>
 <body>
-    <div class="container" style="margin-top: 50px; max-width: 600px;">
+    <div id="form-container" class="container" style="max-width: 600px;">
         <div class="panel panel-primary">
             <div class="panel-heading">
                 <h3 class="panel-title">Compte à rebours</h3>
             </div>
             <div class="panel-body">
-                <?php
-                    // Si le bouton de soumission a été pressé
-                    if(isset($_POST['submit']))
-                    {
-                        // Obtenir le mois, le jour et l'année à partir du champ de formulaire (jj/mm/aaaa)
-                        $date = explode('/', $_POST['date']);
-                        $jour = $date[0];
-                        $mois = $date[1];
-                        $annee = $date[2];
-                        
-                        // Obtenir les secondes futures (heure, minute, seconde, mois, jour, année);
-                        $futur = mktime(00, 00, 00, $mois, $jour, $annee);
-                        
-                        // Obtenir les secondes actuelles depuis l'Unix Epoch (minuit le 1er janvier 1970)
-                        $actuel = time();
-                        
-                        // Calculer la différence en secondes
-                        $difference = $futur - $actuel;
-                        
-                        // Calculer le nombre de jours
-                        $nbjours = ceil($difference / 86400);
-                        
-                        // Vérifier si l'événement a lieu aujourd'hui
-                        if($nbjours > 0)
-                        {
-                            echo "<h1>Il reste $nbjours jours avant {$_POST['event']} !</h1>";
-                        } elseif ($nbjours == 0)  {
-                            echo "<h1>{$_POST['event']} a lieu aujourd'hui !</h1>";
-                        } else {
-                            echo "<h1>{$_POST['event']} a déjà eu lieu !</h1>";    
-                        }
-                    }
-                ?>
-                    
-                <form method="post" action="">
+                <form>
                     <div class="form-group">
                         <label>Nom de l'événement :</label>
                         <input name="event" type="text" class="form-control" required/>
@@ -80,10 +92,11 @@
                         <input id="datepicker" name="date" type="text" class="form-control" required/>
                     </div>
         
-                    <input name="submit" type="submit" value="Calculer les jours" class="btn btn-primary" />
+                    <input type="submit" value="Calculer les jours" class="btn btn-primary" />
                 </form>
             </div>
         </div>
     </div>
+    <div id="countdown" style="display: none;"></div>
 </body>
 </html>
