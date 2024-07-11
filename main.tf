b9f9b3e9155912e2a7e52bb196170d80ebdb653e
Joris <contact@hydeos.fr>
2024-07-11 19:59:46 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "azurerm_service_plan" "TP_Azure" {
 }
 
 resource "azurerm_linux_web_app" "linux-azure" {
-  name                = "tp-azure-ja"
+  name                = "tp-azure-jpab"
   resource_group_name = azurerm_resource_group.Azure.name
   location            = "westeurope"
   service_plan_id     = azurerm_service_plan.TP_Azure.id
